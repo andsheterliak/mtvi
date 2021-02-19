f3ad360e13f882bfd44f3f9956095729d7c397b7
Andrii Sheterliak <andsheterliak@gmail.com>
2021-02-19 18:57:27 +0200

    feat: add responsive margin to RouteHeader

--- src/modules/common/components/RouteHeader.js
+++ src/modules/common/components/RouteHeader.js
@@ -1,11 +1,15 @@
 import { Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
   header: {
     margin: '20px 0 20px 20px',
+
+    [theme.breakpoints.up('md')]: {
+      margin: '40px 0 40px 20px',
+    },
   },
-});
+}));
 
 const RouteHeader = ({ routeName }) => {
   const classes = useStyles();
