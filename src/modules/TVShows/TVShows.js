03bdae6f57d29f53714dcb7abaea77bdcc74c34d
Andrii Sheterliak <andsheterliak@gmail.com>
2021-03-02 18:36:28 +0200

    feat: add UI & fake data to TVShows page

--- src/modules/TVShows/TVShows.js
+++ src/modules/TVShows/TVShows.js
@@ -1,7 +1,92 @@
+import { useState } from 'react';
+import ActionsButtons from '../common/components/ActionsButtons';
+import AdjustmentBar from '../common/components/Adjustment/AdjustmentBar';
+import AdjustmentButton from '../common/components/AdjustmentButton';
+import Cards from '../common/components/Cards/Cards';
+import ContentGrid from '../common/components/ContentGrid';
+import LoadMoreBtn from '../common/components/LoadMoreBtn';
+import Modal from '../common/components/Modal';
 import PageContainer from '../common/components/PageContainer';
 
+import {
+  sortByOptions,
+  defaultOptions,
+  tvGenres,
+  tvData,
+} from '../common/fake-data';
+
+const changeGenres = (genresList, genreName) => {
+  return genresList.map((item) => {
+    if (item.name === genreName) {
+      return { name: item.name, isSelected: !item.isSelected };
+    }
+
+    return item;
+  });
+};
+
 const TVShows = ({ routeName }) => {
-  return <PageContainer routeName={routeName} />;
+  const [isModalOpened, setIsModalOpened] = useState(false);
+
+  const [genres, setGenres] = useState(() => {
+    return tvGenres.map((item) => {
+      return { name: item.name, isSelected: false };
+    });
+  });
+
+  const openModalHandler = () => {
+    setIsModalOpened(true);
+  };
+
+  const closeModalHandler = () => {
+    setIsModalOpened(false);
+  };
+
+  const toggleGenreHandler = (e) => {
+    let dataGenre = e.target.dataset.genre;
+
+    if (dataGenre) {
+      setGenres((prevGenres) => {
+        return changeGenres(prevGenres, dataGenre);
+      });
+
+      return;
+    }
+
+    dataGenre = e.target.closest('[data-genre]').dataset.genre;
+
+    setGenres((prevGenres) => {
+      return changeGenres(prevGenres, dataGenre);
+    });
+  };
+
+  return (
+    <PageContainer routeName={routeName}>
+      <AdjustmentButton
+        ariaLAbel="adjust tv shows"
+        openModalHandler={openModalHandler}
+        modal={
+          <Modal
+            isOpened={isModalOpened}
+            closeModalHandler={closeModalHandler}
+            title="Adjust Tv Shows"
+            content={
+              <AdjustmentBar
+                sortByOptions={sortByOptions}
+                defaultOptions={defaultOptions}
+                genres={genres}
+                dateTitle="Air Dates"
+                toggleGenreHandler={toggleGenreHandler}
+              />
+            }
+            actions={<ActionsButtons />}
+          />
+        }
+      />
+      <ContentGrid secondField={<Cards cardsData={tvData.results} />} />
+      <LoadMoreBtn />
+    </PageContainer>
+  );
 };
 
 export default TVShows;
--- src/modules/common/fake-data.js
+++ src/modules/common/fake-data.js
@@ -91,6 +91,73 @@ export const moviesGenres = [
   },
 ];
 
+export const tvGenres = [
+  {
+    id: 10759,
+    name: 'Action & Adventure',
+  },
+  {
+    id: 16,
+    name: 'Animation',
+  },
+  {
+    id: 35,
+    name: 'Comedy',
+  },
+  {
+    id: 80,
+    name: 'Crime',
+  },
+  {
+    id: 99,
+    name: 'Documentary',
+  },
+  {
+    id: 18,
+    name: 'Drama',
+  },
+  {
+    id: 10751,
+    name: 'Family',
+  },
+  {
+    id: 10762,
+    name: 'Kids',
+  },
+  {
+    id: 9648,
+    name: 'Mystery',
+  },
+  {
+    id: 10763,
+    name: 'News',
+  },
+  {
+    id: 10764,
+    name: 'Reality',
+  },
+  {
+    id: 10765,
+    name: 'Sci-Fi & Fantasy',
+  },
+  {
+    id: 10766,
+    name: 'Soap',
+  },
+  {
+    id: 10767,
+    name: 'Talk',
+  },
+  {
+    id: 10768,
+    name: 'War & Politics',
+  },
+  {
+    id: 37,
+    name: 'Western',
+  },
+];
+
 export const moviesData = {
   page: 1,
   results: [
@@ -455,6 +522,354 @@ export const moviesData = {
       video: false,
       vote_average: 5.6,
       vote_count: 1276,
+    },
+  ],
+  total_pages: 500,
+  total_results: 10000,
+};
+
+export const tvData = {
+  page: 1,
+  results: [
+    {
+      backdrop_path: '/lOr9NKxh4vMweufMOUDJjJhCRHW.jpg',
+      first_air_date: '2021-01-15',
+      genre_ids: [10765, 9648, 18],
+      id: 85271,
+      name: 'WandaVision',
+      origin_country: ['US'],
+      original_language: 'en',
+      original_name: 'WandaVision',
+      overview:
+        'Wanda Maximoff and Vision—two super-powered beings living idealized suburban lives—begin to suspect that everything is not as it seems.',
+      popularity: 3605.076,
+      poster_path:
+        'https://image.tmdb.org/t/p/w500/glKDfE6btIRcVB5zrjspRIs4r52.jpg',
+      vote_average: 8.5,
+      vote_count: 6313,
+    },
+    {
+      backdrop_path: '/colNDt09PACkwtCgWILJQ8i6vSR.jpg',
+      first_air_date: '2017-01-26',
+      genre_ids: [9648, 18, 80],
+      id: 69050,
+      name: 'Riverdale',
+      origin_country: ['US'],
+      original_language: 'en',
+      original_name: 'Riverdale',
+      overview:
+        'Set in the present, the series offers a bold, subversive take on Archie, Betty, Veronica and their friends, exploring the surreality of small-town life, the darkness and weirdness bubbling beneath Riverdale’s wholesome facade.',
+      popularity: 1280.038,
+      poster_path:
+        'https://image.tmdb.org/t/p/w500/wRbjVBdDo5qHAEOVYoMWpM58FSA.jpg',
+      vote_average: 8.6,
+      vote_count: 10216,
+    },
+    {
+      backdrop_path: '/uro2Khv7JxlzXtLb8tCIbRhkb9E.jpg',
+      first_air_date: '2010-10-31',
+      genre_ids: [10759, 18, 10765],
+      id: 1402,
+      name: 'The Walking Dead',
+      origin_country: ['US'],
+      original_language: 'en',
+      original_name: 'The Walking Dead',
+      overview:
+        "Sheriff's deputy Rick Grimes awakens from a coma to find a post-apocalyptic world dominated by flesh-eating zombies. He sets out to find his family and encounters many other survivors along the way.",
+      popularity: 1176.389,
+      poster_path:
+        'https://image.tmdb.org/t/p/w500/rqeYMLryjcawh2JeRpCVUDXYM5b.jpg',
+      vote_average: 8,
+      vote_count: 9835,
+    },
+    {
+      backdrop_path: '/hvbGY8PItRa17A9QSpPCdZAD8Ge.jpg',
+      first_air_date: '2021-02-19',
+      genre_ids: [18, 9648],
+      id: 97513,
+      name: 'The Boarding School: Las Cumbres',
+      origin_country: ['ES'],
+      original_language: 'es',
+      original_name: 'El Internado: Las Cumbres',
+      overview:
+        'In an inaccessible place between the mountains and isolated from the world, a school is located next to an old monastery. The students are rebellious and problematic young people who live under the strict and severe discipline imposed by the center to reintegrate them into society. The surrounding forest is home to ancient legends, threats that are still valid and that will immerse them in terrifying adventures.',
+      popularity: 966.99,
+      poster_path:
+        'https://image.tmdb.org/t/p/w500/kl07N07l4XNjXF48oujzWXs40Dw.jpg',
+      vote_average: 7.4,
+      vote_count: 282,
+    },
+    {
+      backdrop_path: '/ta5oblpMlEcIPIS2YGcq9XEkWK2.jpg',
+      first_air_date: '2016-01-25',
+      genre_ids: [80, 10765],
+      id: 63174,
+      name: 'Lucifer',
+      origin_country: ['US'],
+      original_language: 'en',
+      original_name: 'Lucifer',
+      overview:
+        "Bored and unhappy as the Lord of Hell, Lucifer Morningstar abandoned his throne and retired to Los Angeles, where he has teamed up with LAPD detective Chloe Decker to take down criminals. But the longer he's away from the underworld, the greater the threat that the worst of humanity could escape.",
+      popularity: 700.721,
+      poster_path:
+        'https://image.tmdb.org/t/p/w500/4EYPN5mVIhKLfxGruy7Dy41dTVn.jpg',
+      vote_average: 8.5,
+      vote_count: 7681,
+    },
+    {
+      backdrop_path: '/mZjZgY6ObiKtVuKVDrnS9VnuNlE.jpg',
+      first_air_date: '2017-09-25',
+      genre_ids: [18],
+      id: 71712,
+      name: 'The Good Doctor',
+      origin_country: ['US'],
+      original_language: 'en',
+      original_name: 'The Good Doctor',
+      overview:
+        "A young surgeon with Savant syndrome is recruited into the surgical unit of a prestigious hospital. The question will arise: can a person who doesn't have the ability to relate to people actually save their lives",
+      popularity: 698.394,
+      poster_path:
+        'https://image.tmdb.org/t/p/w500/6tfT03sGp9k4c0J3dypjrI8TSAI.jpg',
+      vote_average: 8.6,
+      vote_count: 6942,
+    },
+    {
+      backdrop_path: '/TXaR5xmz2ohHgnpf0YPWtZPlCO.jpg',
+      first_air_date: '2021-02-23',
+      genre_ids: [10759, 10765],
+      id: 95057,
+      name: 'Superman & Lois',
+      origin_country: ['US'],
+      original_language: 'en',
+      original_name: 'Superman & Lois',
+      overview:
+        "After years of facing megalomaniacal supervillains, monsters wreaking havoc on Metropolis, and alien invaders intent on wiping out the human race, The Man of Steel aka Clark Kent and Lois Lane come face to face with one of their greatest challenges ever: dealing with all the stress, pressures and complexities that come with being working parents in today's society.",
+      popularity: 691.641,
+      poster_path:
+        'https://image.tmdb.org/t/p/w500/6SJppowm7cdQgLkvoTlnTUSbjr9.jpg',
+      vote_average: 8.6,
+      vote_count: 222,
+    },
+    {
+      backdrop_path: '/edmk8xjGBsYVIf4QtLY9WMaMcXZ.jpg',
+      first_air_date: '2005-03-27',
+      genre_ids: [18],
+      id: 1416,
+      name: "Grey's Anatomy",
+      origin_country: ['US'],
+      original_language: 'en',
+      original_name: "Grey's Anatomy",
+      overview:
+        'Follows the personal and professional lives of a group of doctors at Seattle’s Grey Sloan Memorial Hospital.',
+      popularity: 640.486,
+      poster_path:
+        'https://image.tmdb.org/t/p/w500/clnyhPqj1SNgpAdeSS6a6fwE6Bo.jpg',
+      vote_average: 8.2,
+      vote_count: 5391,
+    },
+    {
+      backdrop_path: '/fRYwdeNjMqC30EhofPx5PlDpdun.jpg',
+      first_air_date: '2018-10-25',
+      genre_ids: [10765, 18],
+      id: 79460,
+      name: 'Legacies',
+      origin_country: ['US'],
+      original_language: 'en',
+      original_name: 'Legacies',
+      overview:
+        'In a place where young witches, vampires, and werewolves are nurtured to be their best selves in spite of their worst impulses, Klaus Mikaelson’s daughter, 17-year-old Hope Mikaelson, Alaric Saltzman’s twins, Lizzie and Josie Saltzman, among others, come of age into heroes and villains at The Salvatore School for the Young and Gifted.',
+      popularity: 611.916,
+      poster_path:
+        'https://image.tmdb.org/t/p/w500/qTZIgXrBKURBK1KrsT7fe3qwtl9.jpg',
+      vote_average: 8.6,
+      vote_count: 1640,
+    },
+    {
+      backdrop_path: '/jC1KqsFx8ZyqJyQa2Ohi7xgL7XC.jpg',
+      first_air_date: '2014-10-07',
+      genre_ids: [18, 10765],
+      id: 60735,
+      name: 'The Flash',
+      origin_country: ['US'],
+      original_language: 'en',
+      original_name: 'The Flash',
+      overview:
+        'After a particle accelerator causes a freak storm, CSI Investigator Barry Allen is struck by lightning and falls into a coma. Months later he awakens with the power of super speed, granting him the ability to move through Central City like an unseen guardian angel. Though initially excited by his newfound powers, Barry is shocked to discover he is not the only "meta-human" who was created in the wake of the accelerator explosion -- and not everyone is using their new powers for good. Barry partners with S.T.A.R. Labs and dedicates his life to protect the innocent. For now, only a few close friends and associates know that Barry is literally the fastest man alive, but it won\'t be long before the world learns what Barry Allen has become...The Flash.',
+      popularity: 589.212,
+      poster_path:
+        'https://image.tmdb.org/t/p/w500/lJA2RCMfsWoskqlQhXPSLFQGXEJ.jpg',
+      vote_average: 7.6,
+      vote_count: 6796,
+    },
+    {
+      backdrop_path: '/iqmJfU6DkqJra9PqIepaOJbzfa1.jpg',
+      first_air_date: '2021-01-08',
+      genre_ids: [99],
+      id: 114695,
+      name: 'Marvel Studios: Legends',
+      origin_country: ['US'],
+      original_language: 'en',
+      original_name: 'Marvel Studios: Legends',
+      overview:
+        'Revisit the epic heroes, villains and moments from across the MCU in preparation for the stories still to come. Each dynamic segment feeds directly into the upcoming series — setting the stage for future events. This series weaves together the many threads that constitute the unparalleled Marvel Cinematic Universe.',
+      popularity: 478.823,
+      poster_path:
+        'https://image.tmdb.org/t/p/w500/EpDuYIK81YtCUT3gH2JDpyj8Qk.jpg',
+      vote_average: 7.6,
+      vote_count: 302,
+    },
+    {
+      backdrop_path: '/gL8myjGc2qrmqVosyGm5CWTir9A.jpg',
+      first_air_date: '2018-05-02',
+      genre_ids: [10759, 18],
+      id: 77169,
+      name: 'Cobra Kai',
+      origin_country: ['US'],
+      original_language: 'en',
+      original_name: 'Cobra Kai',
+      overview:
+        'This Karate Kid sequel series picks up 30 years after the events of the 1984 All Valley Karate Tournament and finds Johnny Lawrence on the hunt for redemption by reopening the infamous Cobra Kai karate dojo. This reignites his old rivalry with the successful Daniel LaRusso, who has been working to maintain the balance in his life without mentor Mr. Miyagi.',
+      popularity: 471.643,
+      poster_path:
+        'https://image.tmdb.org/t/p/w500/obLBdhLxheKg8Li1qO11r2SwmYO.jpg',
+      vote_average: 8.1,
+      vote_count: 2622,
+    },
+    {
+      backdrop_path: '/aq2yEMgRQBPfRkrO0Repo2qhUAT.jpg',
+      first_air_date: '2013-03-03',
+      genre_ids: [10759, 18],
+      id: 44217,
+      name: 'Vikings',
+      origin_country: ['CA'],
+      original_language: 'en',
+      original_name: 'Vikings',
+      overview:
+        "The adventures of Ragnar Lothbrok, the greatest hero of his age. The series tells the sagas of Ragnar's band of Viking brothers and his family, as he rises to become King of the Viking tribes. As well as being a fearless warrior, Ragnar embodies the Norse traditions of devotion to the gods. Legend has it that he was a direct descendant of Odin, the god of war and warriors.",
+      popularity: 452.882,
+      poster_path:
+        'https://image.tmdb.org/t/p/w500/bQLrHIRNEkE3PdIWQrZHynQZazu.jpg',
+      vote_average: 8,
+      vote_count: 4119,
+    },
+    {
+      backdrop_path: '/p7fwOnlxYYlB4A8U2b0JfX21Rr1.jpg',
+      first_air_date: '2019-11-12',
+      genre_ids: [10765, 10759, 37, 18],
+      id: 82856,
+      name: 'The Mandalorian',
+      origin_country: ['US'],
+      original_language: 'en',
+      original_name: 'The Mandalorian',
+      overview:
+        'After the fall of the Galactic Empire, lawlessness has spread throughout the galaxy. A lone gunfighter makes his way through the outer reaches, earning his keep as a bounty hunter.',
+      popularity: 429.916,
+      poster_path:
+        'https://image.tmdb.org/t/p/w500/sWgBv7LV2PRoQgkxwlibdGXKz1S.jpg',
+      vote_average: 8.5,
+      vote_count: 5917,
+    },
+    {
+      backdrop_path: '/v2moumGovVAmDASYkjakIl2WrOM.jpg',
+      first_air_date: '2016-09-23',
+      genre_ids: [18, 10759],
+      id: 67133,
+      name: 'MacGyver',
+      origin_country: ['US'],
+      original_language: 'en',
+      original_name: 'MacGyver',
+      overview:
+        '20-something Angus MacGyver creates a clandestine organization where he uses his knack for solving problems in unconventional ways to help prevent disasters from happening.',
+      popularity: 417.579,
+      poster_path:
+        'https://image.tmdb.org/t/p/w500/2zAogx9dmSAu2HYxbWzHe4ZaNY5.jpg',
+      vote_average: 7.1,
+      vote_count: 554,
+    },
+    {
+      backdrop_path: '/suopoADq0k8YZr4dQXcU6pToj6s.jpg',
+      first_air_date: '2011-04-17',
+      genre_ids: [10765, 18, 10759],
+      id: 1399,
+      name: 'Game of Thrones',
+      origin_country: ['US'],
+      original_language: 'en',
+      original_name: 'Game of Thrones',
+      overview:
+        "Seven noble families fight for control of the mythical land of Westeros. Friction between the houses leads to full-scale war. All while a very ancient evil awakens in the farthest north. Amidst the war, a neglected military order of misfits, the Night's Watch, is all that stands between the realms of men and icy horrors beyond.",
+      popularity: 389.393,
+      poster_path:
+        'https://image.tmdb.org/t/p/w500/u3bZgnGQ9T01sWNhyveQz0wH0Hl.jpg',
+      vote_average: 8.4,
+      vote_count: 13213,
+    },
+    {
+      backdrop_path: '/k7T9xRyzP41wBVNyNeLmh8Ch7gD.jpg',
+      first_air_date: '2009-09-10',
+      genre_ids: [18, 10765],
+      id: 18165,
+      name: 'The Vampire Diaries',
+      origin_country: ['US'],
+      original_language: 'en',
+      original_name: 'The Vampire Diaries',
+      overview:
+        'The story of two vampire brothers obsessed with the same girl, who bears a striking resemblance to the beautiful but ruthless vampire they knew and loved in 1864.',
+      popularity: 368.81,
+      poster_path:
+        'https://image.tmdb.org/t/p/w500/aBkVgChtyyJaHyZh1gfd8DbzQon.jpg',
+      vote_average: 8.3,
+      vote_count: 5291,
+    },
+    {
+      backdrop_path: '/hBYvHCcZX4dyvXeM0NTBh5uJIk3.jpg',
+      first_air_date: '2019-01-08',
+      genre_ids: [18],
+      id: 80006,
+      name: 'Good Trouble',
+      origin_country: ['US'],
+      original_language: 'en',
+      original_name: 'Good Trouble',
+      overview:
+        'After moving to The Coterie in Downtown Los Angeles, Callie and Mariana Foster realize that living on their own is not all that it’s cracked up to be.',
+      popularity: 367.656,
+      poster_path:
+        'https://image.tmdb.org/t/p/w500/b9OwPQZr4dVv7K8SrUbAYJ35uLg.jpg',
+      vote_average: 8,
+      vote_count: 87,
+    },
+    {
+      backdrop_path: '/uY3MJohUpY7UMMTK90BIhX5txEs.jpg',
+      first_air_date: '2017-04-30',
+      genre_ids: [18, 9648, 10765],
+      id: 46639,
+      name: 'American Gods',
+      origin_country: ['US'],
+      original_language: 'en',
+      original_name: 'American Gods',
+      overview:
+        "An ex-con becomes the traveling partner of a conman who turns out to be one of the older gods trying to recruit troops to battle the upstart deities. Based on Neil Gaiman's fantasy novel.",
+      popularity: 338.737,
+      poster_path:
+        'https://image.tmdb.org/t/p/w500/3KCAZaKHmoMIN9dHutqaMtubQqD.jpg',
+      vote_average: 7.1,
+      vote_count: 952,
+    },
+    {
+      backdrop_path: '/oAkpvVvoyZWMo9cpNk3aQoVKsjM.jpg',
+      first_air_date: '2016-07-19',
+      genre_ids: [10759, 80, 18, 9648],
+      id: 67335,
+      name: 'Sin senos sí hay paraíso',
+      origin_country: ['CO'],
+      original_language: 'es',
+      original_name: 'Sin senos sí hay paraíso',
+      overview:
+        'A young woman born in a mafia-controlled town seeks revenge on those who wrongfully harmed and imprisoned her family.',
+      popularity: 338.176,
+      poster_path:
+        'https://image.tmdb.org/t/p/w500/ulcGG2JKjbVMQFIux1vcQGN1pQi.jpg',
+      vote_average: 7.5,
+      vote_count: 1897,
     },
   ],
   total_pages: 500,
