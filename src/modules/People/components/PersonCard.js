96ede63c0bcbda9af654b462d4f38ceaa2ca1060
Andrii Sheterliak <andsheterliak@gmail.com>
2021-03-15 17:24:37 +0200

    feat: add fallback img to PersonCard

--- src/modules/People/components/PersonCard.js
+++ src/modules/People/components/PersonCard.js
@@ -7,9 +7,15 @@ import {
 } from '@material-ui/core';
 
 import useStyles from '../../common/components/Cards/CardStyles';
+import { IMG_BASE_URL, IMG_SIZES } from '../../common/tmdb-config';
+import noUserPhotoImg from '../../../assets/img/no-user-photo.svg';
 
 const PersonCard = ({ profilePath, name, knownFor }) => {
   const classes = useStyles();
+
+  const profileImg = profilePath
+    ? `${IMG_BASE_URL}${IMG_SIZES.profile}${profilePath}`
+    : noUserPhotoImg;
 
   const knownForTitles = knownFor
     .map((item) => item.original_name || item.title)
@@ -18,12 +24,7 @@ const PersonCard = ({ profilePath, name, knownFor }) => {
   return (
     <Card raised className={classes.card}>
       <CardActionArea className={classes.action}>
-        <CardMedia
-          component="img"
-          alt={name}
-          image={profilePath}
-          title={name}
-        />
+        <CardMedia component="img" alt={name} image={profileImg} title={name} />
 
         <CardContent className={classes.content}>
           <Typography gutterBottom variant="body1" component="h2">
--- src/modules/People/components/PersonCards.js
+++ src/modules/People/components/PersonCards.js
@@ -1,15 +1,12 @@
 import CardsGridItem from '../../common/components/Cards/CardsGridItem';
-import { IMG_BASE_URL, IMG_SIZES } from '../../common/tmdb-config';
 import PersonCard from './PersonCard';
 
 const PersonCards = ({ cardsData }) => {
   const cards = cardsData.map((item) => {
-    const profilePath = `${IMG_BASE_URL}${IMG_SIZES.profile}${item.profile_path}`;
-
     return (
       <CardsGridItem key={item.id}>
         <PersonCard
-          profilePath={profilePath}
+          profilePath={item.profile_path}
           name={item.name}
           knownFor={item.known_for}
         />
