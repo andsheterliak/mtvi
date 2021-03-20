95c5004e85b3130878e880665b7b8f04ba979ea6
Andrii Sheterliak <andsheterliak@gmail.com>
2021-03-20 18:57:31 +0200

    refactor: set specific routes to links
    * To get rid of conditions, the more routes the more conditions

--- src/modules/Menu/components/Links.js
+++ src/modules/Menu/components/Links.js
@@ -3,25 +3,25 @@ import { Link } from 'react-router-dom';
 import { ListItem, ListItemText } from '@material-ui/core';
 
 const Links = ({ location, routes, ...props }) => {
-  const links = Object.values(routes).map(({ name, to }) => {
-    if (to === '/') return null;
-
-    return (
-      <ListItem
-        button
-        selected={location.pathname === to}
-        component={Link}
-        to={to}
-        key={name}
-        {...props}
-      >
-        <ListItemText
-          primaryTypographyProps={{ color: 'textPrimary' }}
-          primary={name}
-        />
-      </ListItem>
-    );
-  });
+  const links = [routes.movies, routes.tv, routes.people].map(
+    ({ name, to }) => {
+      return (
+        <ListItem
+          button
+          selected={location.pathname === to}
+          component={Link}
+          to={to}
+          key={name}
+          {...props}
+        >
+          <ListItemText
+            primaryTypographyProps={{ color: 'textPrimary' }}
+            primary={name}
+          />
+        </ListItem>
+      );
+    }
+  );
 
   return links;
 };
