9bfb76db5fe2391cdee64da984878a667f6eac65
Andrii Sheterliak <andsheterliak@gmail.com>
2021-04-07 23:13:28 +0300

    refactor: move InfoItem to InfoList

--- src/modules/common/components/PageHeader/InfoList.js
+++ src/modules/common/components/PageHeader/InfoList.js
@@ -1,6 +1,5 @@
 import { makeStyles } from '@material-ui/core/styles';
-
-import InfoItem from './InfoItem';
+import { Typography } from '@material-ui/core';
 
 const useStyles = makeStyles(() => {
   return {
@@ -9,6 +8,12 @@ const useStyles = makeStyles(() => {
       margin: 0,
       padding: 0,
     },
+
+    infoItem: {
+      '&:not(:last-child)': {
+        margin: '0 0 8px 0',
+      },
+    },
   };
 });
 
@@ -16,7 +21,17 @@ const InfoList = ({ dataList }) => {
   const classes = useStyles();
 
   const items = dataList.map(({ name, value }) => {
-    return <InfoItem key={name} name={name} value={value} />;
+    return (
+      <li key={name} className={classes.infoItem}>
+        <Typography component="span" variant="body2" color="textSecondary">
+          {name}:{' '}
+        </Typography>
+
+        <Typography component="span" variant="body2">
+          {value}
+        </Typography>
+      </li>
+    );
   });
 
   return <ul className={classes.list}>{items}</ul>;
