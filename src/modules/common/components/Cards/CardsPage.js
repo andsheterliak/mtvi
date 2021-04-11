0dd451aa6be083ccf6a9001b22ec6ecd0d915437
Andrii Sheterliak <andsheterliak@gmail.com>
2021-04-11 18:24:41 +0300

    docs: write better explanation comment to CardsPage

--- src/modules/common/components/Cards/CardsPage.js
+++ src/modules/common/components/Cards/CardsPage.js
@@ -1,6 +1,11 @@
 import { memo } from 'react';
 
-// This component is only for optimization. Each 'CardsComponent' only runs if 'pageData' - (object with new data on load more action) changes because of each 'CardsComponent' is wrapped in memo.
+/**
+ * This component is only for adding new cards in optimized way.
+ * Each 'CardsComponent' won't re-render (to virtual dom) because each 'CardsComponent' is wrapped in 'memo'.
+ * 'cardsData' reference remains the same as each page of cards data is appended to the previous one, so 'memo' works properly because it compares object references.
+ */
+
 const CardsPage = ({ data, CardsComponent, path }) => {
   return data.map(({ pageData, pageNum }) => {
     return <CardsComponent key={pageNum} cardsData={pageData} path={path} />;
