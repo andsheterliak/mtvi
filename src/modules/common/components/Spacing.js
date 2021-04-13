ffbc96568f57c3374cae908f8047eeb4b6c8dfa8
Andrii Sheterliak <andsheterliak@gmail.com>
2021-04-13 18:36:35 +0300

    Revert "feat: use margins instead of paddings in Spacing"
    * This reverts commit 778f8399f44a83e76e658273e9145668f6f78726.
    * Margins doesn't work because of 'margin collapsing'

--- src/modules/common/components/Spacing.js
+++ src/modules/common/components/Spacing.js
@@ -3,10 +3,10 @@ import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles((theme) => {
   return {
     spacing: {
-      margin: `${theme.spacing(2)}px 0`,
+      padding: `${theme.spacing(2)}px 0`,
 
       [theme.breakpoints.up('sm')]: {
-        margin: `${theme.spacing(3)}px 0`,
+        padding: `${theme.spacing(3)}px 0`,
       },
     },
   };
