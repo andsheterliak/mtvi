99381619a9b5eb1dacf2eebaade0626d56cc60ba
Andrii Sheterliak <andsheterliak@gmail.com>
2021-04-17 23:23:31 +0300

    feat: remove useless styles
    * I use CSS grid insead of Grid component which is based on flexbox

--- src/modules/common/components/MainContainer.js
+++ src/modules/common/components/MainContainer.js
@@ -1,20 +1,7 @@
 import { Container } from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-
-const useStyles = makeStyles({
-  container: {
-    overflow: 'hidden', // To fix grid margins.
-  },
-});
 
 const MainContainer = ({ children }) => {
-  const classes = useStyles();
-
-  return (
-    <Container maxWidth="xl" className={classes.container}>
-      {children}
-    </Container>
-  );
+  return <Container maxWidth="xl">{children}</Container>;
 };
 
 export default MainContainer;
