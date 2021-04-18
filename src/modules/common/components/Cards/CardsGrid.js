60ffe7bf30114b395609fa7997e0fa863885cfce
Andrii Sheterliak <andsheterliak@gmail.com>
2021-04-19 00:39:04 +0300

    fix: incorrect column sizes

--- src/modules/common/components/Cards/CardsGrid.js
+++ src/modules/common/components/Cards/CardsGrid.js
@@ -3,7 +3,7 @@ import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles((theme) => ({
   grid: {
     display: 'grid',
-    gridTemplateColumns: 'repeat(auto-fill, minmax(150px, 1fr))',
+    gridTemplateColumns: '1fr 1fr',
     gap: '15px',
 
     [theme.breakpoints.up('sm')]: {
