0282d777712e2adc29560f562c49bb0c0f26998c
Andrii Sheterliak <andsheterliak@gmail.com>
2021-04-20 00:03:26 +0300

    fix: section overflow when using CSS Grid container

--- src/modules/common/components/Section/Section.js
+++ src/modules/common/components/Section/Section.js
@@ -1,5 +1,17 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(() => {
+  return {
+    section: {
+      width: '100%',
+    },
+  };
+});
+
 const Section = ({ children }) => {
-  return <section>{children}</section>;
+  const classes = useStyles();
+
+  return <section className={classes.section}>{children}</section>;
 };
 
 export default Section;
