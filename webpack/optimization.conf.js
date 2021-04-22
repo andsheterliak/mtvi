4cd0b953f5c983e25a9fd31bcfbc46b002a03077
Andrii Sheterliak <andsheterliak@gmail.com>
2021-04-22 18:57:36 +0300

    docs: remove unimportant comments

--- webpack/optimization.conf.js
+++ webpack/optimization.conf.js
@@ -4,19 +4,10 @@ const optimizationConf = {
   moduleIds: 'hashed', // performance: fix hashing
   // runtimeChunk: true, // Keep the runtime chunk separated to enable long term caching. (Useful if there is more than one entry).
 
-  /**
-   * -------- splitChunks info:
-   * If you have a dynamic imported file and it requires a package from node_modules: such package also will be splitted, if you don't want to, just exclude it.
-   */
   splitChunks: {
     chunks: 'all', // Include all types of chunks async (dynamic imports) and initial (node_modules).
 
-    /**
-     * -------- name info:
-     * Since the chunk name includes all origin chunk names it’s recommended for production builds with long term caching to NOT include [name] in the filenames, or switch off name generation via optimization.splitChunks.name: false. Elsewise files will invalidate i. e. when more chunks with the same vendors are added.
-     *
-     * Disable names for chunks, example: polyfills~app.7d473b584b576a22c8ee.js --> 1.7d473b584b576a22c8ee.js.
-     */
+    // Disable names for chunks, example: polyfills~app.7d473b584b576a22c8ee.js --> 1.7d473b584b576a22c8ee.js.
     name: false,
 
     /**
