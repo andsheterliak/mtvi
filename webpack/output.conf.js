39e4d5f88157e49df21fb52dd3635c8417ee026c
Andrii Sheterliak <andsheterliak@gmail.com>
2021-04-22 19:09:49 +0300

    docs: improve 'publicPath' explanation

--- webpack/output.conf.js
+++ webpack/output.conf.js
@@ -3,7 +3,13 @@ const { paths } = require('./helpers.conf');
 const outputConf = {
   filename: paths.scripts.output.filename,
   path: paths.dist,
-  publicPath: paths.publicPath, // Info: https://www.programmersought.com/article/923487066/
+  
+  /**
+   * Server will use this path as a base path.
+   * Example: "publicPath: '/base' --> https://domain.tld/base/home".
+   * More Info: https://www.programmersought.com/article/923487066/.
+   */
+  publicPath: paths.publicPath,
 };
 
 module.exports = outputConf;
