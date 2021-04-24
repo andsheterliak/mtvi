7e766970a66c805c482002a5908f8c77ea8d73af
Andrii Sheterliak <andsheterliak@gmail.com>
2021-04-24 20:52:14 +0300

    build: change alias prefix

--- webpack/resolve.conf.js
+++ webpack/resolve.conf.js
@@ -7,11 +7,11 @@ const resolveConf = {
   alias: {
     // To replace standard react-dom with @hot-loader/react-dom
     'react-dom': '@hot-loader/react-dom',
-    '@': paths.src,
-    '@assets': `${paths.src}/${dirNames.assets}`,
-    '@modules': `${paths.src}/${dirNames.modules}`,
-    '@common': `${paths.src}/${dirNames.modules}/${dirNames.common}`,
-    '@components': `${paths.src}/${dirNames.modules}/${dirNames.common}/${dirNames.components}`,
+    '~': paths.src,
+    '~assets': `${paths.src}/${dirNames.assets}`,
+    '~modules': `${paths.src}/${dirNames.modules}`,
+    '~common': `${paths.src}/${dirNames.modules}/${dirNames.common}`,
+    '~components': `${paths.src}/${dirNames.modules}/${dirNames.common}/${dirNames.components}`,
   },
 };
 
