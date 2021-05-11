134e6f30806efc83802f6d6dc1cf5e7478d179dc
Andrii Sheterliak <andsheterliak@gmail.com>
2021-05-11 03:48:10 +0300

    feat: improve slider
    * Fix incorrect disabling interaction (again)
    * Add container to set cursor pointer instead of using '*' selector

--- src/common/components/Slider.js
+++ src/common/components/Slider.js
@@ -9,10 +9,6 @@ const useStyles = makeStyles(() => ({
     userSelect: 'none',
     display: 'flex',
 
-    '& *': {
-      cursor: 'pointer',
-    },
-
     // Set fake side paddings, (normal paddings don't work properly in container with "overflow: 'auto'").
     '&:after, &:before': {
       content: '""',
@@ -21,13 +17,22 @@ const useStyles = makeStyles(() => ({
       minWidth: '10px', // "width" doesn't work in container with "overflow: 'auto'".
     },
   },
+
+  container: {
+    cursor: 'pointer',
+  },
 }));
+
+const ifDisableInteraction = (howFarX, howFarY) => {
+  return Math.abs(howFarX) > 0 || Math.abs(howFarY) > 0;
+};
 
 const Slider = ({ children, acceleration = 2 }) => {
   const classes = useStyles();
 
   const sliderRef = useRef(null);
-  const sliderElementsRef = useRef(null);
+  const sliderInnerRef = useRef(null);
+  const isInteractionDisabledRef = useRef(false);
   const isSwipingRef = useRef(false);
 
   const startPositionsRef = useRef({
@@ -37,30 +42,23 @@ const Slider = ({ children, acceleration = 2 }) => {
     clientY: 0,
   });
 
-  useEffect(() => {
-    sliderElementsRef.current = sliderRef.current.querySelectorAll(
-      ':scope >:first-child > *'
-    );
-  }, []);
+  const disableInteraction = (howFarX, howFarY) => {
+    if (isInteractionDisabledRef.current) return;
 
-  const disableInteracting = () => {
-    if (isSwipingRef.current) return;
-
-    sliderElementsRef.current.forEach((item) => {
-      item.style.pointerEvents = 'none';
-    });
+    if (ifDisableInteraction(howFarX, howFarY)) {
+      sliderInnerRef.current.style.pointerEvents = 'none';
+      isInteractionDisabledRef.current = true;
+    }
   };
 
-  const enableInteracting = () => {
-    sliderElementsRef.current.forEach((item) => {
-      item.style.pointerEvents = 'unset';
-    });
+  const enableInteraction = () => {
+    if (isInteractionDisabledRef.current) {
+      sliderInnerRef.current.style.pointerEvents = 'unset';
+      isInteractionDisabledRef.current = false;
+    }
   };
 
   const moveSliderHandler = (e) => {
-    disableInteracting();
-    isSwipingRef.current = true;
-
     const slider = sliderRef.current;
     const startPositions = startPositionsRef.current;
 
@@ -73,9 +71,13 @@ const Slider = ({ children, acceleration = 2 }) => {
 
     slider.scrollTop = startPositions.scrollTop - accelerationY;
     slider.scrollLeft = startPositions.scrollLeft - accelerationX;
+
+    disableInteraction(howFarX, howFarY);
+    isSwipingRef.current = true;
   };
 
   const stopSliderHandler = () => {
+    enableInteraction();
     isSwipingRef.current = false;
 
     document.removeEventListener('mousemove', moveSliderHandler);
@@ -99,6 +101,14 @@ const Slider = ({ children, acceleration = 2 }) => {
     document.addEventListener('mouseup', stopSliderHandler);
   };
 
+  useEffect(() => {
+    const slider = sliderRef.current;
+
+    sliderInnerRef.current = slider.querySelector(
+      ':scope >:first-child >:first-child'
+    );
+  }, []);
+
   return (
     // This is a native scrollable element, so it doesn't need additional attributes I think?!.
     // eslint-disable-next-line jsx-a11y/no-static-element-interactions
@@ -107,7 +117,7 @@ const Slider = ({ children, acceleration = 2 }) => {
       onMouseDown={initSliderHandler}
       className={classes.slider}
     >
-      {children}
+      <div className={classes.container}>{children}</div>
     </div>
   );
 };
