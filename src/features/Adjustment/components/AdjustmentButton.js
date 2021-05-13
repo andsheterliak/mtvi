ec19c83d7d8e82072d27b7c9630bd9328069a427
Andrii Sheterliak <andsheterliak@gmail.com>
2021-05-13 20:12:58 +0300

    feat: use Fab component instead

--- src/features/Adjustment/components/AdjustmentButton.js
+++ src/features/Adjustment/components/AdjustmentButton.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { IconButton } from '@material-ui/core';
+import { Fab } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { MovieFilter as FilterIcon } from '@material-ui/icons/';
 
@@ -9,11 +9,8 @@ import HideOnScroll from '~components/HideOnScroll';
 const useStyles = makeStyles((theme) => {
   return {
     btnWrapper: {
-      backgroundColor: theme.palette.background.paper,
-      boxShadow: theme.shadows[2],
       position: 'fixed',
       bottom: '20px',
-      borderRadius: '50%',
       zIndex: theme.zIndex.speedDial,
     },
   };
@@ -38,16 +35,16 @@ const AdjustmentButton = ({ ariaLabel, openModalHandler }) => {
   return (
     <>
       <HideOnScroll direction={'up'}>
-        <div
+        <Fab
           className={classes.btnWrapper}
+          onClick={openModalHandler}
+          aria-label={ariaLabel}
           style={{
             left: `${windowWidth - 100}px`, // To avoid shifting when scrollbar is hidden.
           }}
         >
-          <IconButton onClick={openModalHandler} aria-label={ariaLabel}>
-            <FilterIcon fontSize="large" />
-          </IconButton>
-        </div>
+          <FilterIcon fontSize="large" />
+        </Fab>
       </HideOnScroll>
     </>
   );
