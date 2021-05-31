8c101b0c1521f47012c56e807ad12d2ab74615ea
Andrii Sheterliak <andsheterliak@gmail.com>
2021-05-31 19:50:46 +0300

    feat: add tv show credits page

---
+++ src/features/TVShow/TVShowCredits.js
@@ -0,0 +1,53 @@
+import noImageImg from '~assets/img/no-image.svg';
+import { ROUTE_NAMES } from '~common/constants';
+import useScrollToTop from '~common/hooks/useScrollToTop';
+import { IMG_BASE_URL, IMG_SIZES } from '~common/tmdb-config';
+import { checkIfIsData, createCreditsData } from '~common/utils/getData';
+
+import BackToHeader from '~components/BackToHeader';
+import Credits from '~components/Credits';
+
+import useTVShowsState from './hooks/useTVShowsState';
+
+const MovieCredits = () => {
+  useScrollToTop();
+
+  const { data } = useTVShowsState();
+
+  let posterImg;
+  let credits;
+
+  if (data) {
+    const isCredits = Object.values(data.aggregate_credits).some((item) => {
+      return checkIfIsData(item);
+    });
+
+    if (isCredits) credits = createCreditsData(data.aggregate_credits);
+
+    posterImg = data.poster_path
+      ? `${IMG_BASE_URL}${IMG_SIZES.poster}${data.poster_path}`
+      : noImageImg;
+  }
+
+  return (
+    <>
+      {data ? (
+        <Credits
+          credits={credits}
+          header={
+            <BackToHeader
+              title={data.title || data.name}
+              imgPath={posterImg}
+              path={`${ROUTE_NAMES.tvShow}/${data.id}`}
+              linkName={`Back to TV Show`}
+            />
+          }
+        />
+      ) : (
+        'Loading'
+      )}
+    </>
+  );
+};
+
+export default MovieCredits;
--- src/features/TVShow/tvShowRoutes.js
+++ src/features/TVShow/tvShowRoutes.js
@@ -1,10 +1,17 @@
 import { ROUTE_NAMES } from '~common/constants';
 import TVShow from '.';
+import TVShowCredits from './TVShowCredits';
 
 const tvShowRoutes = {
   tvShow: {
     to: `${ROUTE_NAMES.tvShow}/:id`,
     component: TVShow,
+    exact: true,
+  },
+
+  tvShowCredits: {
+    to: `${ROUTE_NAMES.tvShow}/:id/${ROUTE_NAMES.credits}`,
+    component: TVShowCredits,
     exact: true,
   },
 };
