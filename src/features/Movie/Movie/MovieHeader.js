421b3c5d858a1e37c845f60be863ec2691683cc3
Andrii Sheterliak <andsheterliak@gmail.com>
2021-06-12 00:55:55 +0300

    fix: incorrect check if is data

--- src/features/Movie/Movie/MovieHeader.js
+++ src/features/Movie/Movie/MovieHeader.js
@@ -3,6 +3,8 @@ import { createSelector } from '@reduxjs/toolkit';
 
 import { formatDataStr, formatMinutes } from '~common/utils/date';
 import { ifIsData, getCertification, getGenres } from '~common/utils/getData';
+import { ROUTE_NAMES } from '~common/constants';
+import { IMG_BASE_URL, IMG_SIZES } from '~common/tmdb-config';
 import Certification from '~components/header/Certification';
 import Creators from '~components/header/Creators';
 import PageHeader from '~components/PageHeader';
@@ -28,8 +30,9 @@ const getDataList = createSelector(getData, (data) => {
   );
 
   let directors = getDirectors(data.credits?.crew);
-
-  directors = directors && <Creators creators={directors} />;
+  directors = ifIsData(directors) ? (
+    <Creators creators={directors} routeName={ROUTE_NAMES.person} />
+  ) : null;
 
   const releaseDate = formatDataStr(data.release_date)?.dateStr;
   const genres = getGenres(data.genres);
@@ -54,10 +57,15 @@ const MovieHeader = () => {
 
   return (
     <PageHeader
-      backdrop={data.backdrop_path}
       overview={data.overview}
       title={data.title}
       dataList={dataList}
+      imgData={{
+        basePath: IMG_BASE_URL,
+        size: IMG_SIZES.backdrop,
+        path: data.backdrop_path,
+        fallback: null,
+      }}
     />
   );
 };
