1752f747ffa556b3e4965aa69aef2e716bc09369
Andrii Sheterliak <andsheterliak@gmail.com>
2021-06-29 04:35:30 +0300

    feat: set roboto font

--- src/index.js
+++ src/index.js
@@ -5,6 +5,10 @@ import { Provider } from 'react-redux';
 import App from './features/App';
 import store from './store';
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 import './index.css';
 
 const renderApp = () => {
