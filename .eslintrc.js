f06a8614f20dc1894273215ef2b479bff4277e73
Andrii Sheterliak <andsheterliak@gmail.com>
2021-08-13 18:07:13 +0300

    build: disable `no-useless-constructor` rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -106,6 +106,7 @@ module.exports = {
         '@typescript-eslint/no-non-null-assertion': 'off',
         '@typescript-eslint/explicit-module-boundary-types': 'off',
         '@typescript-eslint/no-unused-expressions': noUnusedExpressions,
+        'no-useless-constructor': 'off', // Disable to add support for 'Parameter Properties' (shorthand for constructor parameters).
       },
     },
   ],
